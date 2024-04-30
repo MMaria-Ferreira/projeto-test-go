8039eb843a1143d3ace1485ee86f78bf5d48b3a8
Mariana Cruz <mariana.cruz@g.globo>
2024-04-30 09:54:23 -0300

    remove if e adciona switch

--- ola.go
+++ ola.go
@@ -5,19 +5,22 @@ import ()
 const frances = "frances"
 const espanhol = "espanhol"
 const prefixoOlaFrances = "Bonjour"
-const prefixoOlaPortuges = "Olá, "
+const prefixoOlaPortugues = "Olá, "
 const prefixoOlaEspanhol = "Hola, "
 
 func Ola(nome string, idioma string) string {
 	if nome == "" {
 		nome = "Mundo"
 	}
-	if idioma == "Espanhol" {
-		return prefixoOlaEspanhol + nome
-	}
-	if idioma == frances {
-		return prefixoOlaFrances + nome
+	prefixo := prefixoOlaPortugues
+
+	switch idioma {
+	case frances:
+		prefixo = prefixoOlaFrances
+	case espanhol:
+		prefixo = prefixoOlaEspanhol
 
 	}
-	return prefixoOlaEspanhol + nome
+	return prefixo + nome
+
 }
