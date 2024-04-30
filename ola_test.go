eaa72d9b15c9bda2144eb3f6783d4e55b038e3ff
Mariana Cruz <mariana.cruz@g.globo>
2024-04-30 09:22:17 -0300

    adiciona hola, Elodie

--- ola_test.go
+++ ola_test.go
@@ -3,10 +3,18 @@ package main
 import "testing"
 
 func TestOla(t *testing.T) {
-	resultado := Ola("Mari")
-	esperado := "Olá, Mari"
+	verificaMensagemCorreta := func(t *testing.T, resultado, esperado string) {
+		t.Helper()
+		if resultado != esperado {
+			t.Errorf("resultado '%s', esperado '%s'", resultado, esperado)
 
-	if resultado != esperado {
-		t.Errorf("resultado '%s', esperado '%s'", resultado, esperado)
+		}
 	}
+
+	t.Run("em espanhol", func(t *testing.T) {
+		resultado := Ola("Elodie", "espanhol")
+		esperado := "Hola, Elodie"
+		verificaMensagemCorreta(t, resultado, esperado)
+	})
+
 }
